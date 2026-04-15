31602655ad9f5340bcd70d9c9aa172ba1606fae1
FelBenini <felbenini@gmail.com>
2026-04-15 12:13:21 -0300

    :recycle: REFACTOR: Norminetting the code

--- includes/parser.h
+++ includes/parser.h
@@ -6,27 +6,27 @@
 /*   By: fbenini- <fbenini-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 16:32:15 by fbenini-          #+#    #+#             */
-/*   Updated: 2026/04/14 17:53:28 by fbenini-         ###   ########.fr       */
+/*   Updated: 2026/04/15 12:10:48 by fbenini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-#include "scene.h"
+# include "scene.h"
 # include <stdint.h>
 
-t_scene	parse_scene(char *filename);
+t_scene			parse_scene(char *filename);
 
-uint8_t	validate_file(char *filename, int *fd);
+uint8_t			validate_file(char *filename, int *fd);
 
-typedef	uint8_t	(*t_dispatched_fn)(char **values, t_scene *scene);
+typedef uint8_t	(*t_dispatched_fn)(char **values, t_scene *scene);
 
 typedef struct s_dict_dispatcher
 {
 	char			*key;
 	t_dispatched_fn	function;
-}	t_dict_dispatcher;
+}				t_dict_dispatcher;
 
 t_dispatched_fn	dispatch(char **values, t_scene *scene);
 
--- includes/scene.h
+++ includes/scene.h
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   scene.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbenini- <fbenini-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 12:08:38 by fbenini-          #+#    #+#             */
+/*   Updated: 2026/04/15 12:08:43 by fbenini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SCENE_H
 # define SCENE_H
 
-#include "libft.h"
-#include "vec.h"
+# include "libft.h"
+# include "vec.h"
 
 typedef struct s_ambient
 {
