94a696abc1ebcea49094901d616558f4f772c839
Jovane Castro <jovanecastro12@gmail.com>
2026-04-15 19:41:57 -0300

    :sparkles: FEAT: light and camera objects parsing functions

--- srcs/parser/dispatcher/dispatcher.c
+++ srcs/parser/dispatcher/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: jode-cas <jode-cas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 12:08:28 by fbenini-          #+#    #+#             */
-/*   Updated: 2026/04/15 19:19:50 by jode-cas         ###   ########.fr       */
+/*   Updated: 2026/04/15 19:41:28 by jode-cas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ t_dispatched_fn	dispatch(char **values, t_scene *scene)
 	int							len;
 	static t_dict_dispatcher	dict[] = {
 	{"A", parse_ambient},
-	{"C", parse_ambient},
-	{"L", parse_ambient},
+	{"C", parse_camera},
+	{"L", parse_light},
 	{"sp", parse_ambient},
 	{"pl", parse_ambient},
 	{"cy", parse_ambient},
--- srcs/parser/dispatcher/parse_required.c
+++ srcs/parser/dispatcher/parse_required.c
@@ -6,7 +6,7 @@
 /*   By: jode-cas <jode-cas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 19:19:53 by jode-cas          #+#    #+#             */
-/*   Updated: 2026/04/15 19:22:46 by jode-cas         ###   ########.fr       */
+/*   Updated: 2026/04/15 19:40:47 by jode-cas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,27 @@ uint8_t	parse_camera(char **values, t_scene *scene)
 {
 	if (string_array_length(values) != 8)
 		return (0);
+	scene->camera.position.x = ft_atof(values[1]);
+	scene->camera.position.y = ft_atof(values[2]);
+	scene->camera.position.z = ft_atof(values[3]);
+	scene->camera.direction.x = ft_atof(values[4]);
+	scene->camera.direction.y = ft_atof(values[5]);
+	scene->camera.direction.z = ft_atof(values[6]);
+	scene->camera.fov = ft_atof(values[7]);
 }
 
 uint8_t	parse_light(char **values, t_scene *scene)
 {
+	t_vec	color;
+
 	if (string_array_length(values) != 8)
 		return (0);
+	scene->light.position.x = ft_atof(values[1]);
+	scene->light.position.y = ft_atof(values[2]);
+	scene->light.position.z = ft_atof(values[3]);
+	scene->light.intensity = ft_atof(values[4]);
+	color.x = ft_atof(values[5]);
+	color.y = ft_atof(values[6]);
+	color.z = ft_atof(values[7]);
+	scene->light.color = color;
 }
